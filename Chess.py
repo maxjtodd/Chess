f8de63a5b8f8752cf6fd623b8a8045509512fa31
maxjtodd <maxtodd@mail.com>
2023-03-13 18:21:53 -0600

    Created chess controller, changed piece type enum
    Piece type enum now stores negative values for black pieces and positive values for white pieces. Visual aspects renamed to ChessVisual.py, chess controller stored in Chess.py

--- Chess.py
+++ Chess.py
@@ -1,101 +1,37 @@
-import tkinter as tk
-from enum import Enum
-import Pieces
+from Pieces import *
 
 class Chess:
 
-    # Define constant size of chess board size
     BOARD_SIZE = 8
 
     def __init__(self) -> None:
-
-        # Create and draw tiles for window for the board
-        self.window = Chess.drawBoard()
-
-        self.setPiece(1, 1, 1)
-        self.movePiece(1,1,2,3)
-
-        self.window.bind("<Button-1>", self.click)
-
-
-    def drawBoard() -> tk:
-        """
-        Returns a tiled window chess board
-        """
-
-        # Create the window
-        window = tk.Tk(screenName="Chess",baseName="Chess")
-
-
-        # Create grids
-        for i in range(Chess.BOARD_SIZE):
-
-            for j in range(Chess.BOARD_SIZE):
-
-                # draws alternating colors
-                color = "black"
-                if (i + j) % 2 == 0:
-                    color = "white"
-                
-                # Add the grid square to the window
-                newFrame = tk.Frame(master=window, width=100, height=100, bg=color)
-                newFrame.grid(row=i, column=j)            
-
-        return window
-    
-
-
-
-
-    def setPiece(self, piece : int, newX : int, newY : int):
-        """
-        Set a given piece using Piece enum to the new x and y position on the grid.
-        """
-        label = tk.Label(self.window, text=piece)
-        label.grid(row=newY, column=newX)
-
-    
-    def movePiece(self, oldX : int, oldY : int, newX : int, newY : int):
-        """
-        Move the piece from one position to another
-        """
-
-        # Get piece on grid
-        pieceToMove = self.window.grid_slaves(row=oldY, column=oldX)[0]
-        piece = pieceToMove.cget("text")
         
-        # Remove piece to move
-        pieceToMove.grid_remove()
+        self.board = Chess.initializeGame()
 
-        # Create the piece at the new position
-        label = tk.Label(self.window, text=piece)
-        label.grid(row=newY, column=newX)
 
-    
-    
-    def click(self, event):
+    def initializeGame():
+        """
+        Create the iniital state of chess
+        - 0 stores empty space
+        - Other numbers are stored according to PieceType enum values
+        """
+        board = [
+                    [PieceType.BLACKROOK.value, PieceType.BLACKKNIGHT.value, PieceType.BLACKBISHOP.value, PieceType.BLACKQUEEN.value, PieceType.BLACKKING.value, PieceType.BLACKBISHOP.value, PieceType.BLACKKNIGHT.value, PieceType.BLACKROOK.value],
+                    [PieceType.BLACKPAWN.value, PieceType.BLACKPAWN.value, PieceType.BLACKPAWN.value, PieceType.BLACKPAWN.value, PieceType.BLACKPAWN.value, PieceType.BLACKPAWN.value, PieceType.BLACKPAWN.value, PieceType.BLACKPAWN.value],
+                    [0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0],
+                    [PieceType.WHITEPAWN.value, PieceType.WHITEPAWN.value, PieceType.WHITEPAWN.value, PieceType.WHITEPAWN.value, PieceType.WHITEPAWN.value, PieceType.WHITEPAWN.value, PieceType.WHITEPAWN.value, PieceType.WHITEPAWN.value],
+                    [PieceType.WHITEROOK.value, PieceType.WHITEKNIGHT.value, PieceType.WHITEBISHOP.value, PieceType.WHITEQUEEN.value, PieceType.WHITEKING.value, PieceType.WHITEBISHOP.value, PieceType.WHITEKNIGHT.value, PieceType.WHITEROOK.value]
+                ]
         
-        # Here retrieving the size of the parent
-        # widget relative to master widget
-        x = event.x_root - self.window.winfo_rootx()
-        y = event.y_root - self.window.winfo_rooty()
+        return board
     
-        # Here grid_location() method is used to
-        # retrieve the relative position on the
-        # parent widget
-        z = self.window.grid_location(x, y)
-    
-        # printing position
-        print(z)
+    def printBoard(self):
+        for row in self.board:
+            print(row)
 
 
-
-
-def main():
-
-    c = Chess()
-
-    c.window.mainloop()
-
-if __name__ == "__main__":
-    main()
+c = Chess()
+c.printBoard()
--- Pieces.py
+++ Pieces.py
@@ -3,20 +3,34 @@ from enum import Enum
 
 class PieceType(Enum):
     """
-    Declares the piece type enum.
-    - Pawn: 1
-    - Knight: 2
-    - Bishop: 3
-    - Rook: 4
-    - Queen: 5
-    - King: 6
+    Declares the piece type enum. Pawn: 1, Knight: 2, Bishop: 3, Rook: 4, Queen: 5, King: 6. 
+    Number is positive if it's a white piece and is negative if it's a black piece.
+    - WHITEPAWN
+    - WHITEKNIGHT
+    - WHITEBISHOP
+    - WHITEROOK
+    - WHITEQUEEN
+    - WHITEKING
+    - BLACKPAWN
+    - BLACKKNIGHT
+    - BLACKBISHOP
+    - BLACKROOK
+    - BLACKQUEEN
+    - BLACKKING
     """
-    PAWN = 1
-    KNIGHT = 2
-    BISHOP = 3
-    ROOK = 4
-    QUEEN = 5
-    KING = 6
+    WHITEPAWN = 1
+    WHITEKNIGHT = 2
+    WHITEBISHOP = 3
+    WHITEROOK = 4
+    WHITEQUEEN = 5
+    WHITEKING = 6
+    BLACKPAWN = -1
+    BLACKKNIGHT = -2
+    BLACKBISHOP = -3
+    BLACKROOK = -4
+    BLACKQUEEN = -5
+    BLACKKING = -6
+
 
 
 class Piece:
